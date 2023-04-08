d573917a0bb45407fb0df071d73d7c977236bc2a
cp457 <christianpriolo96@gmail.com>
2023-04-08 15:06:08 +0200

    db-conn-2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,31 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+
+
+        ArrayList<String> surnameOutput = new ArrayList<>();
 
         try{
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb", "developer", "developer");
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from student");
+
             while(resultSet.next()){
+
                 System.out.println(resultSet.getString("name"));
-                System.out.println(resultSet.getString("surname"));
+                surnameOutput.add(resultSet.getString("surname"));
             }
+            System.out.println(surnameOutput);
+
 
         }catch (Exception e){
             e.printStackTrace();
 
         }
+        //System.out.println(surnameOutput);
 
     }
 }
